6393c839761b086f2b4999d1b12e25f9f4994d6f
shivamgarg-ti <shivam.garg@trilogy.com>
2022-08-09 14:47:09 +0000

    disable crsf coz it doesnt work

--- django_rest/settings.py
+++ django_rest/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-okn$c17zn^lcap7v@45uadid5@gv9or92-2hn-olra%z#o^*a$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 
